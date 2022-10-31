2e28af3efeb80b35c0a8317f101d85853f40ea85
gazuo <111868974+gazuo@users.noreply.github.com>
2022-10-31 21:46:32 +0800

    Delete config.php

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = 'localhost';
-$username = 'xiaotu11';
-$password = 'ioJL48uaFT';
-$dbname = 'xiaotu11';
-$dbport = '3306';
-$conn = new mysqli($servername, $username, $password, $dbname,$dbport); 
-?>
